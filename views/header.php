f69d706216f9747864dfeeb4c2e4a96d8384dd84
Bradley Ryan <bradryan13@gmail.com>
2014-02-18 16:18:19 -0700

    Base Commmit

---
+++ views/header.php
@@ -0,0 +1,17 @@
+<header>
+
+	<h1 id="logo" href="http://localhost:8888/mls"><img src="img/mls-logo.png"></h1>		
+	
+	<nav id="main-menu">	
+		<ul>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+		</ul>
+	</nav>			
+
+</header>
